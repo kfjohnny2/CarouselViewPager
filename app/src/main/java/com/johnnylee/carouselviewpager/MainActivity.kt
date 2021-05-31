2262567000f3b5eee5af83bb1ac2a77da4576cd4
RafaRamos <rafaelsramos95@gmail.com>
2021-05-31 17:41:01 +0100

    add tooltip dialog

--- app/src/main/java/com/johnnylee/carouselviewpager/MainActivity.kt
+++ app/src/main/java/com/johnnylee/carouselviewpager/MainActivity.kt
@@ -1,129 +1,23 @@
 package com.johnnylee.carouselviewpager
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.johnnylee.carouselviewpager.databinding.ActivityMainBinding
-import com.johnnylee.carouselviewpager.ui.adapter.CarouselViewPagerAdapter
-import com.johnnylee.carouselviewpager.ui.adapter.CarouselViewPagerViewHolder
-import com.johnnylee.carouselviewpager.ui.adapter.with_fragment.CarouselPagerFragment
-import com.johnnylee.carouselviewpager.ui.bottomsheets.BottomSheetHostFragment
-import com.johnnylee.carouselviewpager.ui.bottomsheets.GenericCallback
-import com.johnnylee.carouselviewpager.ui.carousel_items.views.ICarouselViewBinder
-import com.johnnylee.carouselviewpager.ui.page_transformers.SimpleCarouselPageTransformer
-import com.johnnylee.carouselviewpager.ui.utils.extensions.setBackgroundColorFromId
+import com.johnnylee.carouselviewpager.databinding.ActivityMain2Binding
 
 
 /**
  * @author Created by Johnnylee Rocha (kfjohnny2) on 4/16/2021.
  */
 class MainActivity : AppCompatActivity(), LifecycleOwner {
-    private lateinit var binding : ActivityMainBinding
-
-    private val position
-        get() = if (binding.editText.text.toString().isEmpty()) 0 else binding.editText.text.toString().toInt()
-
-    private val viewBinder = object: ICarouselViewBinder<String, CarouselViewPagerViewHolder> {
-
-        override fun getLayoutRes(viewType: Int) = R.layout.item_carousel_selected
-
-        override fun drawUI(holder: CarouselViewPagerViewHolder, item: String, position: Int) {
-            holder.text1.text = "Pos on loaded: $position"
-            holder.textString.text = item
-
-            if (position % 2 == 0) {
-                holder.cardView.setBackgroundColorFromId(R.color.colorPrimary)
-            } else {
-                holder.cardView.setBackgroundColorFromId(R.color.colorSecondary);
-            }
-        }
-
-        override fun instantiateViewHolder(view: View) = CarouselViewPagerViewHolder(view)
-    }
-
-    private lateinit var carouselViewPagerAdapter: CarouselViewPagerAdapter<String, CarouselViewPagerViewHolder>
-    private lateinit var viewPager: ViewPager2
+    private lateinit var binding : ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
 
-        setButtonListeners()
-        setupCarouselAdapter()
-        setBottomSheet()
-    }
-
-    private fun setBottomSheet() {
-        supportFragmentManager.let {
-            BottomSheetHostFragment(CarouselPagerFragment())
-                .setHasCloseIcon(false)
-                .setIsCancelable(true)
-                .withOnDismissListener(object : GenericCallback {
-                    override fun callback() {
-                        Toast.makeText(applicationContext, "Closed bottomsheet", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                })
-                .withCustomBackground(R.color.teal_200, 100F, 100F)
-                .show(it, "MY TAG")
-        }
-    }
-
-    private fun setButtonListeners() {
-        binding.hideBtn.setOnClickListener {
-            carouselViewPagerAdapter.hideItem(position)
-            toast("Removing fragment at position $position")
-        }
-        binding.addBtn.setOnClickListener {
-            carouselViewPagerAdapter.addItem(position, "Test fragment...")
-            toast("Adding fragment at position $position")
-        }
-        binding.updateBtn.setOnClickListener {
-            carouselViewPagerAdapter.updateItem(position, "changed string")
-            toast("Updated fragment at position $position")
-        }
-    }
-
-    private fun setupCarouselAdapter() {
-
-        //------------------ Input try ------------------
-
-        val displayList = mutableListOf(
-            "Frag zero",
-            "Frag one",
-            "Frag two",
-            "Frag three",
-            "Frag four",
-            "Frag five",
-            "Frag six",
-            "Frag seven",
-            "Frag eight",
-            "Frag nine",
-            "Frag ten"
-        )
-
-        //------------------ --------- ------------------
-
-        carouselViewPagerAdapter = CarouselViewPagerAdapter(this, displayList, viewBinder)
-        viewPager = findViewById(R.id.pager)
-        viewPager.adapter = carouselViewPagerAdapter
-        viewPager.offscreenPageLimit = 1
-
-        val pageTransformer = SimpleCarouselPageTransformer(
-            this,
-            R.dimen.viewpager_default_side_item_visibility_width,
-            R.dimen.viewpager_current_item_horizontal_margins
-        )
-        viewPager.setPageTransformer(pageTransformer)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
     }
 
     //--------------- Helper methods ------------------
---
+++ app/src/main/java/com/johnnylee/carouselviewpager/dialogs/utils/ScreenUtils.kt
@@ -0,0 +1,38 @@
+package com.johnnylee.carouselviewpager.dialogs.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Point
+import android.view.View
+import android.view.WindowManager
+
+/**
+ * Copyright (c) $today.year.
+ * Created by Kevin Crimi as part of the ToolTipDialog library published for free usage as determined by the Apache 2.0 license.
+ * https://github.com/kcrimi/ToolTipDialog
+**/
+internal object ScreenUtils {
+
+    fun getPixels(context: Context, dp: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
+    }
+
+    fun getScreenHeight(context: Context): Int {
+        val wm =
+            context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val size = Point()
+        wm.defaultDisplay.getSize(size)
+        return size.y
+    }
+
+    fun bitmapFromView(view: View): Bitmap {
+        val bitmap =
+            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.layout(view.left, view.top, view.right, view.bottom)
+        view.draw(canvas)
+        return bitmap
+    }
+}
